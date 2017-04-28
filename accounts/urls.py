8a2ff6d06efb0ba776afb5bc12d6c200c98cbf3b
Mitesh Shah <mitesh@oxal.org>
2017-04-28 22:20:24 +0530

    fixed ama_host bug, added redirect to login after logging out
    all users were being recognised as a host, added separate css classes
    to diffrentiate

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 
 from . import views
 
@@ -8,7 +9,7 @@ urlpatterns = [
     url(r'^register/', CreateView.as_view(
         template_name='registration/register.html',
         form_class=UserCreationForm,
-        success_url='/'
+        success_url=reverse_lazy('login')
     ), name='register'),
     url(r'', include('django.contrib.auth.urls')),
 ]
