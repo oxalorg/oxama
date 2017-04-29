8caefedd35ab9967b9fc5b76aab34c48fa80553c
Mitesh Shah <mitesh@oxal.org>
2017-04-29 22:46:41 +0530

    added settings for production deployment

---
+++ amasite/production_settings.py
@@ -0,0 +1,11 @@
+import os
+from amasite.settings import *
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ['SECRET_KEY']
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = os.environ['ALLOWED_HOSTS'].split(',')
+
